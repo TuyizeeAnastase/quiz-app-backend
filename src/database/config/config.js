9629b24fa6e96c55599be476350cdbd0bdffe512
Anastase Tuyizere <42033331+TuyizeeAnastase@users.noreply.github.com>
2023-04-20 23:43:48 +0200

    fix-db-config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
